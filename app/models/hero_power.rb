91eb95cbfa56e781bf050df024c64c5696fbe5a9
Steven Otianga <steveotianga8991@gmail.com>
2022-10-10 04:05:05 +0300

    validation for the HeroPower

--- app/models/hero_power.rb
+++ app/models/hero_power.rb
@@ -1,4 +1,8 @@
 class HeroPower < ApplicationRecord
   belongs_to :hero
   belongs_to :power
+
+  strength = [ 'Strong', 'Weak', 'Average']
+   validates :strength, inclusion:{
+    in: strength,
 end
